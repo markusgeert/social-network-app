ad21f773690f996eaa902acaf4af9bd3bea424bf
Max Stevens <maxstevens2708@gmail.com>
2023-12-10 16:41:55 +0100

    Add card scanning page

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, make_response
 
 
 def create_app():
@@ -35,11 +35,19 @@ def create_app():
             "favourite food",
         ]
 
-        return render_template(
-            "index.html",
-            current_user=current_user,
-            matched_user=matched_user,
-            fields=displayed_fields,
+        resp = make_response(
+            render_template(
+                "matching_page.html",
+                current_user=current_user,
+                matched_user=matched_user,
+                fields=displayed_fields,
+            )
         )
+
+        return resp
+
+    @app.get("/startup")
+    def startup():
+        return render_template("card_scanning_page.html")
 
     return app
