0d3cdc1f2d30668bb70a5e1c96741885f3c90fbf
Max Stevens <maxstevens2708@gmail.com>
2023-12-10 14:37:41 +0100

    Format flaskr init

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,36 +1,42 @@
 from flask import Flask, render_template, url_for
 
+
 def create_app():
     app = Flask(__name__)
 
-    @app.get('/')
+    @app.get("/")
     def home():
         users = [
             {
-                'firstname': 'John',
-                'lastname': 'Doe',
-                'avatar_url': url_for('static', filename='profiles/john.png'),
-                'age': 53,
-                'favourite holiday': 'Beach',
-                'favourite season': 'Autumn',
-                'favourite food': 'Pasta' 
+                "firstname": "John",
+                "lastname": "Doe",
+                "avatar_url": url_for("static", filename="profiles/john.png"),
+                "age": 53,
+                "favourite holiday": "Beach",
+                "favourite season": "Autumn",
+                "favourite food": "Pasta",
             },
             {
-                'firstname': 'Mary',
-                'lastname': 'Jane',
-                'avatar_url': url_for('static', filename='profiles/mary.png'),
-                'age': 56,
-                'favourite holiday': 'Beach',
-                'favourite season': 'Spring',
-                'favourite food': 'Pizza' 
-            }
+                "firstname": "Mary",
+                "lastname": "Jane",
+                "avatar_url": url_for("static", filename="profiles/mary.png"),
+                "age": 56,
+                "favourite holiday": "Beach",
+                "favourite season": "Spring",
+                "favourite food": "Pizza",
+            },
         ]
-        displayed_fields = ['age', 'favourite holiday', 'favourite season', 'favourite food']
+        displayed_fields = [
+            "age",
+            "favourite holiday",
+            "favourite season",
+            "favourite food",
+        ]
 
-        return render_template('index.html', users=users, fields=displayed_fields)
+        return render_template("index.html", users=users, fields=displayed_fields)
 
-    @app.get('/admin')
+    @app.get("/admin")
     def admin():
-        return render_template('admin.html')
+        return render_template("admin.html")
 
     return app
