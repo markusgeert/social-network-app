c97b127d77c87685e82e371b0dab4598e7a8c392
Max Stevens <maxstevens2708@gmail.com>
2023-12-12 00:10:59 +0100

    Show user based on the url, for testing

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,5 +1,19 @@
 from flask import Flask, redirect, render_template, request, url_for, make_response
 import csv
+import json
+
+
+def get_user_by_name(name):
+    with open("./data.csv", "r") as file:
+        csv_reader = csv.reader(file)
+        contents = list(csv_reader)
+
+    user_row = None
+    for row in contents:
+        if row[0].lower() == name.lower():
+            user_row = row
+
+    return user_row
 
 
 def get_user(uuid):
@@ -21,35 +35,66 @@ def user_to_dict(user, is_current_user=False):
         "avatar_url": url_for("static", filename=f"profiles/{user[0].lower()}.png"),
         "is_current_user": is_current_user,
         "age": user[1],
+        "screentime": user[2],
         "gender": user[3],
         "favourite music": user[4],
+        "favourite pet": user[5],
+        "favourite food": user[6],
         "favourite holiday": user[8],
         "favourite season": user[7],
-        "favourite food": user[6],
     }
+
+
+def get_user_matches(uuid):
+    with open("./graph.json", "r") as file:
+        graph = json.load(file)
+
+    links_with_user = [
+        link
+        for link in graph["links"]
+        if link["source"] == uuid or link["target"] == uuid
+    ]
+
+    links_with_user.sort(key=lambda link: link["weight"], reverse=True)
+
+    return links_with_user
 
 
 def create_app():
     app = Flask(__name__)
 
-    @app.get("/")
-    def home():
-        user_uuid = request.cookies.get("user")
+    @app.get("/<name>")
+    def home(name):
+        user = get_user_by_name(name)
+        # user_uuid = request.cookies.get("user")
+        # user = get_user(user_uuid)
 
-        user = get_user(user_uuid)
         if user is None:
             return redirect(url_for("login"))
 
+        user_uuid = user[-1]
+
         current_user = user_to_dict(user, is_current_user=True)
-        matched_user = user_to_dict(get_user("3B27A88B-7A99-4E62-A6FA-0B7CCE00D5A8"))
+
+        user_matches = get_user_matches(user_uuid)
+
+        best_match = user_matches[0]
+        if best_match["source"] == user_uuid:
+            matched_user_uuid = best_match["target"]
+        else:
+            matched_user_uuid = best_match["source"]
+
+        matched_user = user_to_dict(get_user(matched_user_uuid))
 
         displayed_fields = [
             "age",
             "gender",
             "favourite music",
             "favourite holiday",
+            "favourite pet",
             "favourite season",
             "favourite food",
+            "screentime",
         ]
 
         resp = make_response(
@@ -63,12 +108,12 @@ def create_app():
 
         return resp
 
-    @app.get("/login")
-    def login():
-        res = redirect(url_for("home"))
-
-        res.set_cookie("user", "AAB5934A-1235-4969-A717-69026733E8A5")
-
-        return res
+    # @app.get("/login")
+    # def login():
+    #     res = redirect(url_for("home"))
+    #
+    #     res.set_cookie("user", "AAB5934A-1235-4969-A717-69026733E8A5")
+    #
+    #     return res
 
     return app
