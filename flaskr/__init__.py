ba575630040d8ad950e14d65d1ea3f63b199872a
Max Stevens <maxstevens2708@gmail.com>
2023-12-11 23:46:29 +0100

    Add data, dynamically retrieve users

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,4 +1,32 @@
-from flask import Flask, render_template, url_for, make_response
+from flask import Flask, redirect, render_template, request, url_for, make_response
+import csv
+
+
+def get_user(uuid):
+    with open("./data.csv", "r") as file:
+        csv_reader = csv.reader(file)
+        contents = list(csv_reader)
+
+    user_row = None
+    for row in contents:
+        if row[-1] == uuid:
+            user_row = row
+
+    return user_row
+
+
+def user_to_dict(user, is_current_user=False):
+    return {
+        "firstname": user[0],
+        "avatar_url": url_for("static", filename=f"profiles/{user[0].lower()}.png"),
+        "is_current_user": is_current_user,
+        "age": user[1],
+        "gender": user[3],
+        "favourite music": user[4],
+        "favourite holiday": user[8],
+        "favourite season": user[7],
+        "favourite food": user[6],
+    }
 
 
 def create_app():
@@ -6,30 +34,19 @@ def create_app():
 
     @app.get("/")
     def home():
-        current_user = {
-            "firstname": "John",
-            "lastname": "Doe",
-            "avatar_url": url_for("static", filename="profiles/john.png"),
-            "is_current_user": True,
-            "age": 53,
-            "favourite holiday": "Beach",
-            "favourite season": "Autumn",
-            "favourite food": "Pasta",
-        }
+        user_uuid = request.cookies.get("user")
 
-        matched_user = {
-            "firstname": "Mary",
-            "lastname": "Jane",
-            "avatar_url": url_for("static", filename="profiles/mary.png"),
-            "is_current_user": False,
-            "age": 56,
-            "favourite holiday": "Beach",
-            "favourite season": "Spring",
-            "favourite food": "Pizza",
-        }
+        user = get_user(user_uuid)
+        if user is None:
+            return redirect(url_for("login"))
+
+        current_user = user_to_dict(user, is_current_user=True)
+        matched_user = user_to_dict(get_user("3B27A88B-7A99-4E62-A6FA-0B7CCE00D5A8"))
 
         displayed_fields = [
             "age",
+            "gender",
+            "favourite music",
             "favourite holiday",
             "favourite season",
             "favourite food",
@@ -46,8 +63,12 @@ def create_app():
 
         return resp
 
-    @app.get("/startup")
-    def startup():
-        return render_template("card_scanning_page.html")
+    @app.get("/login")
+    def login():
+        res = redirect(url_for("home"))
+
+        res.set_cookie("user", "AAB5934A-1235-4969-A717-69026733E8A5")
+
+        return res
 
     return app
